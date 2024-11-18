935c3c64f12cbc6d87f1a3631dca8c68298cc5cb
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-18 10:51:06 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/홀수만 출력하기/print-only-odd-numbers.c
+++ 241118/홀수만 출력하기/print-only-odd-numbers.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n = 0;
-    scanf("%d", &n);
+    int N;
+    scanf("%d", &N); // 첫 번째 줄: N 입력
 
-    int num;
-    for(int i = 1; i <= n; i++) {
-        scanf("%d", &num);
-        
-        if(i % 2 != 0 && i % 3 == 0) {
-            printf("%d\n", i);
-        } 
+    for (int i = 0; i < N; i++) {
+        int num;
+        scanf("%d", &num); // 각 숫자 입력
+        if (num % 6 == 3) { // 홀수이면서 3의 배수인지 간단히 확인
+            printf("%d\n", num); // 조건 만족 시 출력
+        }
     }
     return 0;
 }
