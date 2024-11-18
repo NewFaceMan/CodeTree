2bccd318a7d0ea85f6b51814f2b8903e3af215d9
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-18 10:33:33 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241118/369 게임/369-game.c
+++ 241118/369 게임/369-game.c
@@ -4,13 +4,18 @@ int main() {
     int n = 0;
     scanf("%d", &n);
 
-    for(int i = 0; i <=n; i++) {
+    for(int i = 1; i <=n; i++) {
         if (i == 0) {
             continue;
         }
-        if(i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+        if(i % 3 == 0) {
             printf("0 ");
-        } else {
+        } else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            printf("0 ");
+        } else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+            printf("0 ");
+        }
+        else {
             printf("%d ", i);
         }
     }
