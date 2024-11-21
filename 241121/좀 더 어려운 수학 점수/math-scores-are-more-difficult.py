f9af6715b6cca25c866684d76ee1b59a3ffc098d
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-21 18:03:43 +0900

    Title: 좀 더 어려운 수학 점수, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241121/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 241121/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -2,7 +2,7 @@
 a_math, a_english = map(int, input().split())
 b_math, b_english = map(int, input().split())
 
-if a_math > b_math or (a_math == b_math and a_math > b_math) :
+if a_math > b_math or (a_math == b_math and a_english > b_english) :
     print('A')
 else :
     print('B')
