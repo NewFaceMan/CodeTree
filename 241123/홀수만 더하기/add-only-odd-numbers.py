bfab1824d9a7610398b15173dfdb01891893c7dd
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-23 19:53:17 +0900

    Title: 홀수만 더하기, Time: 91ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241123/홀수만 더하기/add-only-odd-numbers.py
+++ 241123/홀수만 더하기/add-only-odd-numbers.py
@@ -1,9 +1,10 @@
 count = 0
 sum_val = 0
-while count >= 10 :
+while count <= 10 :
     n = int(input())
     if (n % 2 == 1 and b % 3 == 0) :
         sum_val += n
     else :
         continue;
+    count += 1
 print(sum_val)
