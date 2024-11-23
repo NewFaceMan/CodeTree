5244b9d2adfe74798607dbd169cd3f40a3f11440
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-23 16:47:54 +0900

    Title: 코로나 메뉴얼, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241123/코로나 메뉴얼/covid-manual.py
+++ 241123/코로나 메뉴얼/covid-manual.py
@@ -10,47 +10,13 @@ c = input().split()
 c_coldSymptoms = c[0]
 c_temperature = int(c[1])
 
-clinicA = 0
-clinicB = 0
-clinicC = 0
-clinicD = 0
-
-if a_coldSymptoms == 'Y' : #감기 있을 떄
-    if a_temperature >= 37 : #37도 이상이면
-        clinicA += 1 #A + 1
-    else : #37도 이하면
-        clinicC += 1 #C + 1
-else : #감기 없을 때
-    if a_temperature >= 37 : #37도 이상이면
-        clinicB += 1 #B + 1
-    else : # 37도 이하면
-        clinicD += 1 #D + 1
-
-if b_coldSymptoms == 'Y' : #감기 있을 떄
-    if b_temperature >= 37 : #37도 이상이면
-        clinicA += 1 #A + 1
-    else : #37도 이하면
-        clinicC += 1 #C + 1
-else : #감기 없을 때
-    if b_temperature >= 37 : #37도 이상이면
-        clinicB += 1 #B + 1
-    else : # 37도 이하면
-        clinicD += 1 #D + 1
-
-if c_coldSymptoms == 'Y' : #감기 있을 떄
-    if c_temperature >= 37 : #37도 이상이면
-        clinicA += 1 #A + 1
-    else : #37도 이하면
-        clinicC += 1 #C + 1
-else : #감기 없을 때
-    if c_temperature >= 37 : #37도 이상이면
-        clinicB += 1 #B + 1
-    else : # 37도 이하면
-        clinicD += 1 #D + 1
-
-if clinicA >= 2 :
-    print('E')
+if a_coldSymptoms == 'Y' and a_temperature >= 37 :
+    if (b_coldSymptoms == 'Y' and b_temperature >= 37) or (c_coldSymptoms == 'Y' and c_temperature >= 37) :
+        print('E')
+    else :
+        print('N')
 else :
-    print('N')
-        
-
+    if (b_coldSymptoms == 'Y' and b_temperature >= 37) and (c_coldSymptoms == 'Y' and c_temperature >= 37) : 
+        print('E')
+    else :
+        print('N')
