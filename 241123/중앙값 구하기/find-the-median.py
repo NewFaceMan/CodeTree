c31d84d1720a0d0a66c6e6c41ed010d0655c683d
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-23 12:01:36 +0900

    Title: 중앙값 구하기, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 241123/중앙값 구하기/find-the-median.py
+++ 241123/중앙값 구하기/find-the-median.py
@@ -1,16 +1,9 @@
 a, b, c = map(int, input().split())
 
-if a > b and a > c : # a가 제일 클 때
-    if b > c : #b가 c보다 크면
-        print(b) #b출력
-    else : #c가 b보다 크면
-        print(c) 
-
-elif b > a and b > c : # b가 제일 클 때
-    if a > b : #a가 b보다 크면
-        print(a) #b출력
-    else : #b가 a보다 크면
-        print(b)
-
-else :
-    print(c)
+# 가장 큰 값 찾기
+if a > b and a > c:  # a가 가장 큰 경우
+    print(b if b > c else c)
+elif b > a and b > c:  # b가 가장 큰 경우
+    print(a if a > c else c)
+else:  # c가 가장 큰 경우
+    print(a if a > b else b)
