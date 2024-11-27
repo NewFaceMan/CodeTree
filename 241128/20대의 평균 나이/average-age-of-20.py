bb86e97dd2f8f697e12fd5cdb38bd4fd462e4d6d
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2024-11-28 02:25:14 +0900

    Title: 20대의 평균 나이, Time: 83ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241128/20대의 평균 나이/average-age-of-20.py
+++ 241128/20대의 평균 나이/average-age-of-20.py
@@ -1,11 +1,11 @@
-sum = 0
+sum_age = 0
 avg = 0
 cnt = 0
 while True :
     age = int(input())
     
     if age < 30 :
-        sum += age
+        sum_age += age
         cnt += 1
     else :
         avg = sum / cnt
