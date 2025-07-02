1a5686d66142643ac2467ddd60849c63a6e6ac87
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2025-07-03 01:28:02 +0900

    Title: 비교에 따른 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/비교에 따른 연산/operation-based-on-comparison.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf("%d\n", a * b);
+    } else {
+        printf("%d", b / a);
+    }
+    // Please write your code here.
+    return 0;
+}
