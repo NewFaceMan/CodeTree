668c52b7cedf9a73ad3c54214a33de9de95916cf
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2025-07-03 15:31:16 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    int i;
+    scanf("%c", &a);
+    for(i = 0; i < 8; i++) {
+        printf("%c", a);
+    }
+    // Please write your code here.
+    return 0;
+}
