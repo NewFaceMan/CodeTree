a20b8e99a36e14d01e9aed265bc6b39c2aac2c47
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2025-07-03 15:34:04 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/문자열 출력하기/print-string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        printf("LeebrosCode\n");
+    // Please write your code here.
+    return 0;
+}
