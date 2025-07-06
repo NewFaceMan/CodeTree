f27983bb1b0f5effb0f18b5d03f7e2d0613ff05b
ParkDippings <115519562+NewFaceMan@users.noreply.github.com>
2025-07-06 15:17:56 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250706/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n = 1;
+    while (n != 0) {
+        scanf("%d", &n);
+        if (n == 0) {
+            break;
+        }
+        printf("%d\n", n);
+    }
+    // Please write your code here.
+    return 0;
+}
